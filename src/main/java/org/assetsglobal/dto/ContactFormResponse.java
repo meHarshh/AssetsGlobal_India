f548a9e0dd2bb493fe15541b3fcbd49c0510840e
Harsh Kashyap <meharshhk@gmail.com>
2024-09-21 12:17:59 +0530

    updated the compiler

--- src/main/java/org/assetsglobal/dto/ContactFormResponse.java
+++ src/main/java/org/assetsglobal/dto/ContactFormResponse.java
@@ -2,8 +2,7 @@ package org.assetsglobal.dto;
 
 import lombok.Builder;
 
-
-@Builder 
+@Builder
 public class ContactFormResponse {
 
 	private int contactId;
@@ -11,37 +10,57 @@ public class ContactFormResponse {
 	private String email;
 	private String message;
 	private long mobileNumber;
+
 	public int getContactId() {
 		return contactId;
 	}
+
 	public void setContactId(int contactId) {
 		this.contactId = contactId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
 	public long getMobileNumber() {
 		return mobileNumber;
 	}
+
 	public void setMobileNumber(long mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
 
-	
-}
+	public ContactFormResponse() {
+	}
 
+	public ContactFormResponse(int contactId, String name, String email, String message, long mobileNumber) {
+		super();
+		this.contactId = contactId;
+		this.name = name;
+		this.email = email;
+		this.message = message;
+		this.mobileNumber = mobileNumber;
+	}
+
+}
--- src/main/java/org/assetsglobal/dto/HomeLoanResponse.java
+++ src/main/java/org/assetsglobal/dto/HomeLoanResponse.java
@@ -4,7 +4,6 @@ import org.assetsglobal.enums.BankingPartner;
 
 import lombok.Builder;
 
-
 @Builder
 public class HomeLoanResponse {
 	private int loadId;
@@ -12,36 +11,57 @@ public class HomeLoanResponse {
 	private String email;
 	private long phoneNumber;
 	private BankingPartner bankingPartner;
+
 	public int getLoadId() {
 		return loadId;
 	}
+
 	public void setLoadId(int loadId) {
 		this.loadId = loadId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public long getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
 	public BankingPartner getBankingPartner() {
 		return bankingPartner;
 	}
+
 	public void setBankingPartner(BankingPartner bankingPartner) {
 		this.bankingPartner = bankingPartner;
 	}
-	
-	
+
+	public HomeLoanResponse(int loadId, String name, String email, long phoneNumber, BankingPartner bankingPartner) {
+		this.loadId = loadId;
+		this.name = name;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.bankingPartner = bankingPartner;
+	}
+
+	public HomeLoanResponse() {
+		// TODO Auto-generated constructor stub
+	}
+
 }
